661349193cfd44a513e1507e959f67f47306e475
d-kang <david.kang714@gmail.com>
2018-01-30 08:24:05 -0800

    Can Display Player and Opponent Grid

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,65 @@
 import React, { PureComponent } from 'react';
 
+
+const createBoard = () => {
+  return [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ]
+}
+
 class App extends PureComponent {
+  state = {
+    board_1: [],
+    board_2: []
+  }
+
+  componentDidMount() {
+    this.setState({
+      board_1: createBoard(),
+      board_2: createBoard()
+    })
+  }
+
   render() {
     return (
       <div className='App'>
-        hello!
+        <div className="flex-container">
+          <div className="grid-container opponent">
+            {
+              this.state.board_1.map((row, i) => {
+                return (
+                  row.map((cell, k) => {
+                    return (
+                      <div className="grid-item"></div>
+                    )
+                  })
+                )
+              })
+            }
+          </div>
+          <div className="grid-container player">
+            {
+              this.state.board_1.map((row, i) => {
+                return (
+                  row.map((cell, k) => {
+                    return (
+                      <div className="grid-item"></div>
+                    )
+                  })
+                )
+              })
+            }
+          </div>
+        </div>
       </div>
     )
   }
