2fc82c5b981aa288b384b619d29f00120b845317
d-kang <david.kang714@gmail.com>
2018-02-05 11:30:37 -0800

    Update Express Server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const app = module.exports = express();
-const PORT = process.env.PORT || '9090';
 
+app.set('port', (process.env.PORT || 9090));
+const PORT = app.get('port');
+app.use(cors());
 app.use(express.static(path.join(__dirname, '../dist')));
 app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
