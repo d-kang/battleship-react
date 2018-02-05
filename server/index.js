d1ebb8077e04949be5887a441fa55e1f030d4cd2
d-kang <david.kang714@gmail.com>
2018-02-05 10:54:14 -0800

    Setup Express Server to Deploy To Heroku

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = module.exports = express();
+const PORT = process.env.PORT || '8080';
+
+app.use(express.static(path.join(__dirname, '../dist')));
+app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
