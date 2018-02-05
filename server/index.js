da036bc569dfed1d3265a7a11469eb3a02a0cded
d-kang <david.kang714@gmail.com>
2018-02-05 11:22:49 -0800

    Change Port Number

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = module.exports = express();
-const PORT = process.env.PORT || '8080';
+const PORT = process.env.PORT || '9090';
 
 app.use(express.static(path.join(__dirname, '../dist')));
 app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
