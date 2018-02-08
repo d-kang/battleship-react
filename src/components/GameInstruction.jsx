d85f8cdd7fe4e31d8cd5fbb268dc1070b845c807
d-kang <david.kang714@gmail.com>
2018-02-08 11:16:17 -0800

    Add proptypes to GameInstruction

--- src/components/GameInstruction.jsx
+++ src/components/GameInstruction.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Button from './Button';
 
-const GameInstruction = ({ instruction, handleClick, buttonText }) =>
-  <div
-    style={{textAlign: 'center'}}
-  >
+const GameInstruction = ({ instruction, handleClick, buttonText, mode }) =>
+<section id='instructions'>
+  <header className="header_2">Instructions to play against Computer</header>
+  <Button
+  onClick={handleClick}
+  buttonText={buttonText}
+  />
+    <br/>
+    <br/>
+    <br/>
     <div>{instruction}</div>
-    <Button
-      onClick={handleClick}
-      buttonText={buttonText}
-    />
-  </div>
+  </section>;
+
+GameInstruction.propTypes = {
+  instruction: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
+  buttonText: PropTypes.string.isRequired,
+  mode: PropTypes.string.isRequired,
+};
 
 export default GameInstruction;
