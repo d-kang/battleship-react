a47807b8cee43a9c99818d2462e379391fe21cb4
d-kang <david.kang714@gmail.com>
2018-02-08 11:15:59 -0800

    Add GameInstruction to Boards

--- src/components/Boards.jsx
+++ src/components/Boards.jsx
@@ -1,25 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import BoardMap from './BoardMap';
+import GameInstruction from './GameInstruction';
 
-const Boards = ({ player, opponent }) =>
+const Boards = ({ player, opponent, mode, instruction, handleClick, buttonText }) =>
   <div className="container">
     <BoardMap
       board={player}
       isPlayer={true}
       className="board-container player"
+      mode={mode}
     />
     <BoardMap
+      board={opponent}
       isPlayer={false}
       className="board-container opponent"
-      board={opponent}
-      style={{borderTop: '1px'}}
+      mode={mode}
+    />
+    <GameInstruction
+      instruction={instruction}
+      handleClick={handleClick}
+      buttonText={buttonText}
+      mode={mode}
     />
   </div>;
 
 Boards.propTypes = {
   player: PropTypes.array.isRequired,
   opponent: PropTypes.array.isRequired,
+  mode: PropTypes.string.isRequired,
+  instruction: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
+  buttonText: PropTypes.string.isRequired,
 };
 
 export default Boards;
